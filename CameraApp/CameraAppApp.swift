0c845eee192a25fb08f47b36df0de22f95b1d39f
minesyouto <minesyouto8833@gmail.com>
2022-01-31 17:15:50 +0900

    Initial Commit

---
+++ CameraApp/CameraAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraAppApp.swift
+//  CameraApp
+//
+//  Created by みねた on 2022/01/31.
+//
+
+import SwiftUI
+
+@main
+struct CameraAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
